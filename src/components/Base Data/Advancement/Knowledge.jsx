7f711b6a0ceec0feb65a12d89ece093417161685
Krishnaja_A <krishnaja.akella@smartims.com>
2024-05-08 15:24:55 +0530

    Advancements in Insurance

--- src/components/Base Data/Advancement/Knowledge.jsx
+++ src/components/Base Data/Advancement/Knowledge.jsx
@@ -4,15 +4,30 @@ import styles from "./Advancement.module.css";
 
 const Knowledge = () => {
   return (
-    <div className={styles.channelLinks}>
+<div className={styles.channelLinks}>
+
+    <div>
       <Button
         variant="contained"
-        sx={{ backgroundColor: "gray" }}
+        sx={{ backgroundColor: "green" }}
         href="https://smartims.sharepoint.com/:f:/s/InsurancePractice-InsuranceKnowledgeManagement/Eg4PNojOqhZBv_bZ3IhhxgwBPRgpJ9OA1UJ7FO2QP13O0w"
         target="_blank"
       >
         Softskill Developement
       </Button>
+      </div>
+<div>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "green" }}
+        href="https://smartims.sharepoint.com/:f:/s/InsurancePractice-InsuranceKnowledgeManagement/EquDK1fTrD1CmQ7tJVAPrjABP2AKw5Vvup6virt2tl3c5g?e=KOtsYZ"
+        target="_blank"
+      >
+        Line of Business 
+
+      </Button>
+      </div>
+
     </div>
   );
 };
